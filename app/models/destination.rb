2866d35a8783c2e10194f5aabcb8289a581f3167
svrrealism <svrrealists@gmail.com>
2018-01-29 12:10:00 -0500

    Edit comment in destination model, add columns to tags table.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,4 @@
 class Destination < ApplicationRecord
+  #analogous to a Book belonging to an Author.
   belongs_to :tag
 end
--- db/migrate/20180129130215_create_tags.rb
+++ db/migrate/20180129130215_create_tags.rb
@@ -1,7 +1,8 @@
 class CreateTags < ActiveRecord::Migration[5.1]
   def change
     create_table :tags do |t|
-
+      t.string :title
+      t.string :image
       t.timestamps
     end
   end
