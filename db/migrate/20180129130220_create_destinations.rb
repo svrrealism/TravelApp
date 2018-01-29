8d561ff54d4fa763506276419feb8abb63ac3f2a
svrrealism <svrrealists@gmail.com>
2018-01-29 12:16:16 -0500

    Add columns for name, image, description, and tag reference.

--- db/migrate/20180129130220_create_destinations.rb
+++ db/migrate/20180129130220_create_destinations.rb
@@ -1,7 +1,10 @@
 class CreateDestinations < ActiveRecord::Migration[5.1]
   def change
     create_table :destinations do |t|
-
+      t.string :name
+      t.string :image
+      t.description :description
+      t.references :tag
       t.timestamps
     end
   end
