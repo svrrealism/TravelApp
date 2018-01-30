cd192654bb12c55494a1b2cae7b0fd793d149e76
svrrealism <svrrealists@gmail.com>
2018-01-29 20:34:58 -0500

    clarify naming routes comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/tags' => 'tags#index'
-  #Name your routes with 'as'
+  #Name your routes with 'as' function the 'some_arbitrary_tag'
   get '/tags/:id' => 'tags#show', as: :tag
 end
