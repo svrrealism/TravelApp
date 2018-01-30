eed9c90307d534294c134b34d24a8b888dc0e37a
svrrealism <svrrealists@gmail.com>
2018-01-29 22:02:20 -0500

    Create links between individual destinations from specific tags showing all

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tags' => 'tags#index'
+  get '/tags' => 'tags#index', as: :tags
   #Name your routes with 'as' function the 'some_arbitrary_tag'
   get '/tags/:id' => 'tags#show', as: :tag
   get '/destinations/:id' => 'destinations#show', as: :destination
